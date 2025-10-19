57cc0f12dafe8d5fa02f10cd438c4fef1260e0e9
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-10-19 20:13:34 +0200

    remove the beginn of darkmode

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,7 +11,7 @@ const Button = ({ buttonContent, onClick, children }: ButtonProps) => {
     <button
       type="button"
       onClick={onClick}
-      className="mt-5 focus:outline-none text-white bg-emerald-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      className="mt-5 focus:outline-none text-white bg-emerald-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
     >
       {/* falls children existiert, zeig sie; sonst zeig den buttonContent */}
       {children ?? <span>{buttonContent ?? "Beschreibung fehlt"}</span>}
